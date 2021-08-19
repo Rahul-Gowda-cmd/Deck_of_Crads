9d7c3fba2b89c3be1a8a9f3d828a82b3adf4ff4a
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-19 21:55:42 +0530

    feat:Added Deck of Cards program

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display Message
+            Console.WriteLine("Welcome to Deck Of Cards!");
+
+            //Call Class
+            Cards cards = new Cards();
+            cards.CardsCreator();
+        }
+    }
+}
